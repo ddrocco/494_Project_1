ac3f2b9c2d7dac82c6ef8daa5d07efc39340b513
Maia Hoberman <maiayh@umich.edu>
2015-01-21 05:44:27 -0500

    Goddamit I'm done

--- Assets/Scripts/Camera_Adjust.cs
+++ Assets/Scripts/Camera_Adjust.cs
@@ -6,7 +6,7 @@ public class Camera_Adjust : MonoBehaviour {
 	void Awake () {
 		Camera.main.aspect = 1.0f;
 		Camera.main.orthographicSize = 8f;
-		Vector3 newPos = new Vector3(Camera.main.transform.position.x,
+		Vector3 newPos = new Vector3(Camera.main.transform.position.x - .5f,
 									Camera.main.transform.position.y + 8.5f,
 									Camera.main.transform.position.z);
 		Camera.main.transform.position = newPos;
