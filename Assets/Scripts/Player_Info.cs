e530a789fe0f1a107c90bee42b5af63f06972555
Maia Hoberman <maiayh@umich.edu>
2015-01-21 03:18:04 -0500

    script updates part 4

--- Assets/Scripts/Player_Info.cs
+++ Assets/Scripts/Player_Info.cs
@@ -6,7 +6,6 @@ public class Player_Info : MonoBehaviour {
 	public static GameObject gameObj;
 	public Vector3 spawnLocation = new Vector3(-6, 4, 0);
 
-	// Use this for initialization
 	void Awake () {
 		GameObject player = Instantiate (playerPrefab, spawnLocation, Quaternion.identity) as GameObject;
 		gameObj = player;
