2eac692bc16f0f9aa446bc546fa1190c6ffe6f3f
ddrocco <keredoccor@gmail.com>
2015-01-21 05:13:37 -0500

    Made some minor world fixes

--- Assets/Scripts/Generate_L1.cs
+++ Assets/Scripts/Generate_L1.cs
@@ -59,7 +59,7 @@ public class Generate_L1 : MonoBehaviour {
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0));
-		world.Add(new Vector16(4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 1, 1, 1, 0));
+		world.Add(new Vector16(4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 1, 1, 1, 1));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 0, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0));
@@ -68,7 +68,7 @@ public class Generate_L1 : MonoBehaviour {
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-		world.Add(new Vector16(1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0));
+		world.Add(new Vector16(1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 0, 0, 0, 0));
 		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
