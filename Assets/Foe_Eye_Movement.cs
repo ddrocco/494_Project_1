280aa77fd047e21f7b861b8fbf239761ceada9d2
ddrocco <keredoccor@gmail.com>
2015-01-24 17:51:24 -0500

    Made new eye movement work!  It rotates towards each point and then switches targets once it gets there.  Just need to make it track player and move point-locations dynamically with the camera now.

--- Assets/Foe_Eye_Movement.cs
+++ Assets/Foe_Eye_Movement.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class Foe_Eye_Movement : MonoBehaviour {
 	public List<Vector3> pattern;
 	public float velocity = 0.05f;
 	public int currentTarget = 0;
 	public float rotationSpeed = 2f;
+	
+	public Vector3 forwardVector;
 	
 	// Use this for initialization
 	void Start () {
@@ -18,22 +21,23 @@ public class Foe_Eye_Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 direction = (pattern[currentTarget] - transform.position);
-		direction.z = 0f;
-		Quaternion lookRotation = Quaternion.LookRotation(direction);
-		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
-		transform.Translate (velocity * Vector3.forward);
-		transform.position = transform.position.x * Vector3.right + transform.position.y * Vector3.up;
-		/*direction -= direction.z * Vector3.forward;
-		//print (direction.x + " " + direction.y + " " + direction.z);
-		direction = direction.normalized;
-		print (direction.x + " " + direction.y + " " + direction.z);
-		Quaternion lookRotation = Quaternion.LookRotation(-direction);
-		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
-		transform.Translate (velocity * Vector3.up);
-		*/
+		
+		Vector3 locationVector = pattern[currentTarget] - transform.position;
+		
+		transform.rotation = Quaternion.Slerp (transform.rotation,
+				Quaternion.FromToRotation(Vector3.right,locationVector.normalized),
+				rotationSpeed);
+		
+		transform.Translate (velocity * Vector3.right);
+		
 		if (Vector3.Distance(pattern[currentTarget], transform.position) < 0.5f) {
 			currentTarget = (currentTarget + 1) % pattern.Count;
+		}
+		
+		if (Mathf.Abs(transform.rotation.eulerAngles.z - 180f) > 90f) {
+			transform.localScale = new Vector3(1, 1, 1);
+		} else {
+			transform.localScale = new Vector3(1, -1, 1);
 		}
 	}
 }
