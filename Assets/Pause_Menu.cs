7f21e9f04275eb47c1770717a784f9cfc1ebbc50
Maia Hoberman <maiayh@umich.edu>
2015-01-27 22:26:33 -0500

    Tried something else with the rooms; still didn't work. Pause works but only with GUI, no screen-change

---
+++ Assets/Pause_Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause_Menu : MonoBehaviour {
+	bool paused = false;
+	
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			paused = togglePause();
+		}
+	}
+	
+	void OnGUI() {
+		if(paused) {
+			GUILayout.Label("Game is paused!");
+		}
+	}
+	
+	bool togglePause() {
+		if(Time.timeScale == 0f) {
+			Time.timeScale = 1f;
+			return(false);
+		} else {
+			Time.timeScale = 0f;
+			return(true);    
+		}
+	}
+
+}
